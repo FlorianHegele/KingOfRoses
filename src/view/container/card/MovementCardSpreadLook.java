cee297e4966e06e2c2e3ab4b8fbef0230137b412
FlorianHegele <hegele.florian@gmail.com>
2024-04-28 15:26:42 +0200

    add new look

---
+++ src/view/container/card/MovementCardSpreadLook.java
@@ -0,0 +1,12 @@
+package view.container.card;
+
+import boardifier.model.ContainerElement;
+import boardifier.view.TableLook;
+
+public class MovementCardSpreadLook extends TableLook {
+
+    public MovementCardSpreadLook(ContainerElement containerElement) {
+        super(containerElement, -1, 1);
+    }
+
+}
