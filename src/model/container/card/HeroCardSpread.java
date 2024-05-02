d9c427efb23abf58f8f6a403240c8c351479d83a
Florian Hegele <hegele.florian@gmail.com>
2024-05-02 11:36:10 +0200

    set stack

--- src/model/container/card/HeroCardSpread.java
+++ src/model/container/card/HeroCardSpread.java
@@ -7,6 +7,6 @@ public class HeroCardSpread extends ContainerElement {
 
     public HeroCardSpread(int x, int y, GameStageModel gameStageModel) {
         // call the super-constructor to create a 1x1 grid, named "cardstack", and in x,y in space
-        super("cardstack", x, y, 4, 2, gameStageModel);
+        super("cardstack", x, y, 1, 2, gameStageModel);
     }
 }
