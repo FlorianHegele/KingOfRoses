3c5a72370cc01dfb808adcd6761b2482941122d7
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2024-05-02 14:07:25 +0200

    edit test file

---
+++ test/MyTest.java
@@ -0,0 +1,10 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    public void firstTest() {
+        Assertions.assertEquals(1, 1);
+    }
+}
