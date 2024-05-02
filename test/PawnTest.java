f5c6f55d65c2bb7350eba80cc3420962ebb9d158
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2024-05-02 15:56:04 +0200

    100% Pawn Coverage

--- test/PawnTest.java
+++ test/PawnTest.java
@@ -18,8 +18,11 @@ public class PawnTest {
         Pawn pawn = new Pawn(Pawn.Status.BLUE_PAWN,
                 // Création d'un plateau de jeu
                 new KoRStageModel("test", new Model()));
+
         // Vérification que le pion crée est le bon
         assertEquals(Pawn.Status.BLUE_PAWN, pawn.getStatus());
+        // Vérification que le type du pion est bien 50
+        assertEquals(50, pawn.getType());
     }
 
     /*
@@ -33,6 +36,7 @@ public class PawnTest {
                 new KoRStageModel("test", new Model()));
         // Changement de statut du pion
         pawn.flipStatus();
+
         // Vérification du changement de statut
         assertEquals(Pawn.Status.RED_PAWN, pawn.getStatus());
     }
@@ -45,6 +49,7 @@ public class PawnTest {
         Pawn pawnB = new Pawn(Pawn.Status.BLUE_PAWN, stage);
         Pawn pawnR = new Pawn(Pawn.Status.RED_PAWN, stage);
         Pawn pawnY = new Pawn(Pawn.Status.KING_PAWN, stage);
+
         // Teste la récupération de la couleur de fond d'un pion
         assertEquals(BLUE_BACKGROUND, pawnB.getStatus().getBackgroundColor());
         assertEquals(RED_BACKGROUND, pawnR.getStatus().getBackgroundColor());
@@ -58,9 +63,27 @@ public class PawnTest {
         // Création d'un pion bleu et rouge
         Pawn pawnB = new Pawn(Pawn.Status.BLUE_PAWN, stage);
         Pawn pawnR = new Pawn(Pawn.Status.RED_PAWN, stage);
+        
         // Teste la récupération du statut opposé d'un pion
         assertEquals(Pawn.Status.RED_PAWN, pawnB.getStatus().getOpposite());
         assertEquals(Pawn.Status.BLUE_PAWN, pawnR.getStatus().getOpposite());
+    }
+
+    @Test
+    public void testGetID(){
+        // Création d'un plateau
+        KoRStageModel stage = new KoRStageModel("test", new Model());
+        // Création d'un pion bleu, rouge et jaune
+        Pawn pawnB = new Pawn(Pawn.Status.BLUE_PAWN, stage);
+        Pawn pawnR = new Pawn(Pawn.Status.RED_PAWN, stage);
+        Pawn pawnY = new Pawn(Pawn.Status.KING_PAWN, stage);
+
+        // Teste la récupération de l'ID d'un pion
+        // L'ID est l'entier associé à un statut
+        // 0 pour RED_PAWN, 1 pour BLUE_PAWN et 2 pour KING_PAWN
+        assertEquals(1, pawnB.getStatus().getID());
+        assertEquals(0, pawnR.getStatus().getID());
+        assertEquals(2, pawnY.getStatus().getID());
     }
 
 
