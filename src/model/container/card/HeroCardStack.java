889b9445052f1dfd8d112ca463042087ef8d9613
FlorianHegele <hegele.florian@gmail.com>
2024-05-08 00:58:30 +0200

    fix invisible issue

--- src/model/container/card/HeroCardStack.java
+++ src/model/container/card/HeroCardStack.java
@@ -7,6 +7,6 @@ public class HeroCardStack extends ContainerElement {
 
     public HeroCardStack(int x, int y, GameStageModel gameStageModel) {
         // call the super-constructor to create a 1x1 grid, named "cardstack", and in x,y in space
-        super("cardstack", x, y, 1, 2, gameStageModel);
+        super("herocardstack", x, y, 1, 1, gameStageModel);
     }
 }
