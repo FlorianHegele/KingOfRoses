de7cbcd8cb58553c65aa599b90098795fc1186bc
FlorianHegele <hegele.florian@gmail.com>
2024-05-08 00:46:59 +0200

    remove useless code and comment

--- src/model/container/KoRBoard.java
+++ src/model/container/KoRBoard.java
@@ -1,13 +1,7 @@
 package model.container;
 
-import boardifier.model.GameElement;
 import boardifier.model.GameStageModel;
 import boardifier.model.ContainerElement;
-import model.element.Pawn;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.awt.*;
 
 /**
  * KoR main board represent the element where pawns are put when played
@@ -20,24 +14,5 @@ public class KoRBoard extends ContainerElement {
     public KoRBoard(int x, int y, GameStageModel gameStageModel) {
         // call the super-constructor to create a 9x9 grid, named "KoR", and in x,y in space
         super("KoRboard", x, y, 9 , 9, gameStageModel);
-    }
-    // TODO : REWRITE CODE HERE
-    public void setValidCells(Pawn pawn) {
-        resetReachableCells(false);
-        List<Point> valid = computeValidCells(pawn);
-        if (valid != null) {
-            for(Point p : valid) {
-                reachableCells[p.y][p.x] = true;
-            }
-        }
-    }
-    public List<Point> computeValidCells(Pawn pawn) {
-        List<Point> lst = new ArrayList<>();
-         /*
-        TODO:
-            - compute the list of cells that are valid to play taking the pawn value (i.e. number) into account.
-            each Point in this list consists in couple x,y, where x is a column and y a row in the board.
-         */
-        return lst;
     }
 }
