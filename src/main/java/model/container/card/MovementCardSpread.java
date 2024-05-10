2425a11c82a26fa4cb356a12814a70ea4ca0efae
FlorianHegele <hegele.florian@gmail.com>
2024-05-11 00:29:08 +0200

    fix issue with the remove from container action

--- src/main/java/model/container/card/MovementCardSpread.java
+++ src/main/java/model/container/card/MovementCardSpread.java
@@ -6,9 +6,11 @@ import model.PlayerData;
 
 public class MovementCardSpread extends ContainerElement {
 
+    public static final String PREFIX = "movementcardspread_";
+
     public MovementCardSpread(PlayerData playerData, int x, int y, GameStageModel gameStageModel) {
         // call the super-constructor to create a 5x1 grid, named "movementcardspread", and in x,y in space
-        super("movementcardspread_" + playerData.getName(), x, y, 5, 1, gameStageModel);
+        super(PREFIX + playerData.getName(), x, y, 5, 1, gameStageModel);
     }
 
 }
