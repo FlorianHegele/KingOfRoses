b58cbf6811ca90d6a417535a6c78d4b7586c0440
FlorianHegele <hegele.florian@gmail.com>
2024-05-19 02:12:58 +0200

    add documentation for utils classes

--- src/main/java/utils/ContainerElements.java
+++ src/main/java/utils/ContainerElements.java
@@ -4,17 +4,36 @@ import boardifier.model.ContainerElement;
 import boardifier.model.Coord2D;
 import boardifier.model.GameElement;
 
+/**
+ * The {@code ContainerElements} class provides utility methods for working with elements in a container.
+ */
 public class ContainerElements {
 
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
     private ContainerElements() {
         throw new IllegalStateException("Utility class");
     }
 
+    /**
+     * Retrieves the position of a game element within a container element.
+     *
+     * @param gameElement the game element whose position is to be retrieved.
+     * @param containerElement the container element containing the game element.
+     * @return the position of the game element as a {@link Coord2D} object, or null if the element is not found.
+     */
     public static Coord2D getElementPosition(GameElement gameElement, ContainerElement containerElement) {
         final int[] cell = containerElement.getElementCell(gameElement);
         return (cell == null) ? null : new Coord2D(cell[1], cell[0]);
     }
 
+    /**
+     * Retrieves the position of an empty slot within a container element.
+     *
+     * @param containerElement the container element to search for an empty slot.
+     * @return the position of an empty slot as a {@link Coord2D} object, or null if no empty slot is found.
+     */
     public static Coord2D getEmptyPosition(ContainerElement containerElement) {
         for (int row = 0; row < containerElement.getNbRows(); row++) {
             for (int col = 0; col < containerElement.getNbCols(); col++) {
@@ -25,6 +44,12 @@ public class ContainerElements {
         return null;
     }
 
+    /**
+     * Counts the total number of elements stored in a container element.
+     *
+     * @param containerElement the container element whose elements are to be counted.
+     * @return the total number of elements in the container.
+     */
     public static int countElements(ContainerElement containerElement) {
         int count = 0;
         for (int row = 0; row < containerElement.getNbRows(); row++) {
--- src/main/java/utils/Lists.java
+++ src/main/java/utils/Lists.java
@@ -3,18 +3,41 @@ package utils;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * The {@code Lists} class provides utility methods for working with lists.
+ */
 public class Lists {
 
     private static final Random random = new Random();
 
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
     private Lists() {
         throw new IllegalStateException("Utility class");
     }
 
+    /**
+     * Returns a random element from the specified list.
+     *
+     * @param list the list from which to select a random element.
+     * @param random the random number generator to use.
+     * @param <T> the type of elements in the list.
+     * @return a randomly selected element from the list.
+     * @throws IllegalArgumentException if the list is empty.
+     */
     public static <T> T getRandomElement(List<T> list, Random random) {
         return list.get(random.nextInt(list.size()));
     }
 
+    /**
+     * Returns a random element from the specified list using the default random number generator.
+     *
+     * @param list the list from which to select a random element.
+     * @param <T> the type of elements in the list.
+     * @return a randomly selected element from the list.
+     * @throws IllegalArgumentException if the list is empty.
+     */
     public static <T> T getRandomElement(List<T> list) {
         return getRandomElement(list, random);
     }
