b202806c4be3499fdbc39d1d1b37546afc58b4b0
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2024-05-22 14:19:09 +0200

    create ActionsUtils with converter from actionList to int

--- src/test/java/control/ai/TestAiCamarade.java
+++ src/test/java/control/ai/TestAiCamarade.java
@@ -1,9 +1,12 @@
 
 import boardifier.control.StageFactory;
+import boardifier.model.Coord2D;
 import boardifier.model.GameException;
 import boardifier.model.Model;
 import boardifier.model.Player;
 import boardifier.model.action.ActionList;
+import boardifier.model.action.GameAction;
+import boardifier.model.action.RemoveFromContainerAction;
 import boardifier.view.View;
 import control.ConsoleController;
 import control.GameConfigurationController;
@@ -11,13 +14,27 @@ import control.KoRController;
 import control.ai.KoRDeciderCamarade;
 import model.GameConfigurationModel;
 import model.KoRStageModel;
+import model.container.card.MovementCardSpread;
+import model.data.AIData;
+import model.data.PlayerData;
+import model.element.card.MovementCard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import utils.ActionsUtils;
+import utils.ContainerElements;
+import utils.Strings;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TestAiCamarade{
 
     private KoRStageModel stageModel;
+    private GameConfigurationModel gameconfigurationModel;
 
     @BeforeEach
     void createStageModel() throws GameException {
@@ -28,7 +45,7 @@ class TestAiCamarade{
         final Model model = new Model();
 
         // Set up game configuration
-        final GameConfigurationModel gameConfigurationModel = new GameConfigurationModel(model);
+        final GameConfigurationModel gameConfigurationModel = new GameConfigurationModel(model,2,1,true);
         final GameConfigurationController gameConfigurationController = new GameConfigurationController(gameConfigurationModel, consoleController);
         gameConfigurationController.doCheck();
 
@@ -36,6 +53,9 @@ class TestAiCamarade{
         StageFactory.registerModelAndView("kor", "model.KoRStageModel", "view.KoRStageView");
         final View korView = new View(model);
         final KoRController control = new KoRController(model, korView, consoleController, gameConfigurationModel);
+        // Change AI of the first player to CAMARADE for the test needs
+        gameConfigurationModel.addAI(Map.of(PlayerData.PLAYER_BLUE, AIData.RANDOM, PlayerData.PLAYER_RED, AIData.CAMARADE));
+
         control.setFirstStageName("kor");
 
         control.startGame();
@@ -45,5 +65,20 @@ class TestAiCamarade{
 
     @Test
     void testPlateauVide(){
+
+        // Create the AI decider
+        KoRDeciderCamarade aiDecider = new KoRDeciderCamarade(stageModel.getModel(), null, PlayerData.PLAYER_RED);
+
+        // Get the action list from the AI
+        ActionList actionList = aiDecider.decide();
+
+        // Playing a movement card involves the following :
+        // Discarding a card, moving the king, putting a new pawn
+
+        System.out.println(actionList.toString());
+
+        // is the AI playing a movement card ?
+        assertEquals(1,ActionsUtils.actionListToInt(actionList));
+
     }
 }
