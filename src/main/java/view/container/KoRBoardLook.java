a8a5412c16ec501b87de7bbc20201fe87ec3a76a
timcarrara <carrara.tim85@gmail.com>
2024-06-12 16:11:41 +0200

    fix test

--- src/main/java/view/container/KoRBoardLook.java
+++ src/main/java/view/container/KoRBoardLook.java
@@ -2,7 +2,10 @@ package view.container;
 
 import boardifier.model.ContainerElement;
 import boardifier.view.ClassicBoardLook;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
 public class KoRBoardLook extends ClassicBoardLook {
 
@@ -11,5 +14,14 @@ public class KoRBoardLook extends ClassicBoardLook {
         // If we create the 3x3 board by adding a border of 10 pixels, with cells occupying all the available surface,
         // then, cells have a size of (size-20)/3
         super(size / 3, element, -1, Color.BEIGE, Color.DARKGRAY, 0, Color.BLACK, 5, Color.BLACK, false);
+    }
+
+    @Override
+    protected void render() {
+        super.render();
+        Image cardBackground = new Image("file:src/main/ressources/rdr-plateau-vide.png");
+        Rectangle rectangle = new Rectangle(getWidth()+5, getHeight()+5);
+        rectangle.setFill(new ImagePattern(cardBackground));
+        addShape(rectangle);
     }
 }
