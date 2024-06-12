b873e162ee272d005408883f5ddfe8d70a5c1c03
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2024-06-12 16:20:06 +0200

    Add config menu

--- src/main/java/view/KoRView.java
+++ src/main/java/view/KoRView.java
@@ -1,18 +1,21 @@
 package view;
 
+import boardifier.model.GameException;
 import boardifier.model.Model;
 import boardifier.view.RootPane;
 import boardifier.view.View;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import model.GameConfigurationModel;
 
 public class KoRView extends View {
 
     private MenuItem menuStart;
     private MenuItem menuIntro;
     private MenuItem menuQuit;
+    private RadioMenuItem menuHumanVsHuman;
+    private RadioMenuItem menuHumanVsAI;
+    private RadioMenuItem menuAIVsAI;
 
     public KoRView(Model model, Stage stage, RootPane rootPane) {
         super(model, stage, rootPane);
@@ -20,15 +23,40 @@ public class KoRView extends View {
 
     @Override
     protected void createMenuBar() {
+
         menuBar = new MenuBar();
-        Menu menu1 = new Menu("Game");
-        menuStart = new MenuItem("New game");
-        menuIntro = new MenuItem("Intro");
-        menuQuit = new MenuItem("Quit");
-        menu1.getItems().add(menuStart);
-        menu1.getItems().add(menuIntro);
-        menu1.getItems().add(menuQuit);
-        menuBar.getMenus().add(menu1);
+        Menu menu1 = new Menu("Jeu");
+        menuStart = new MenuItem("Nouvelle partie");
+        menuIntro = new MenuItem("Retour au menu principal");
+        menuQuit = new MenuItem("Quitter");
+
+        menu1.getItems().addAll(menuStart, menuIntro, menuQuit);
+
+        Menu menuConfig = new Menu("Configuration");
+        ToggleGroup group = new ToggleGroup();
+        menuHumanVsHuman = new RadioMenuItem("Humain vs Humain");
+        menuHumanVsAI = new RadioMenuItem("Humain vs IA");
+        menuAIVsAI = new RadioMenuItem("IA vs IA");
+
+        menuHumanVsHuman.setToggleGroup(group);
+        menuHumanVsAI.setToggleGroup(group);
+        menuAIVsAI.setToggleGroup(group);
+
+        menuConfig.getItems().addAll(menuHumanVsHuman, menuHumanVsAI, menuAIVsAI);
+
+        menuBar.getMenus().addAll(menu1, menuConfig);
+    }
+
+    public RadioMenuItem getMenuHumanVsHuman() {
+        return menuHumanVsHuman;
+    }
+
+    public RadioMenuItem getMenuHumanVsAI() {
+        return menuHumanVsAI;
+    }
+
+    public RadioMenuItem getMenuAIVsAI() {
+        return menuAIVsAI;
     }
 
     public MenuItem getMenuStart() {
