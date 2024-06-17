d2b5651b39095132227f28baff99a07a884007d8
FlorianHegele <hegele.florian@gmail.com>
2024-06-18 00:13:38 +0200

    trying to write mouse controller

--- src/test/java/control/TestKoRControllerMouse.java
+++ src/test/java/control/TestKoRControllerMouse.java
@@ -1,18 +1,21 @@
 package control;
 
+import boardifier.control.Controller;
 import boardifier.model.Model;
+import javafx.scene.layout.Pane;
 import model.GameConfigurationModel;
 import model.KoRStageModel;
 import model.container.KoRBoard;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import view.KoRView;
 
 class TestKoRControllerMouse {
 
     private KoRStageModel stageModel;
     private Model model;
     private KoRBoard board;
-    private KoRControllerMouse controllerMouse;
 
     @BeforeEach
     void createStageModel() {
@@ -23,9 +26,7 @@ class TestKoRControllerMouse {
 
         stageModel = new KoRStageModel("", model);
         model.setGameStage(stageModel);
-        controllerMouse = new KoRControllerMouse(model, null, null);
 
-        GameConfigurationModel.RANDOM.setSeed(3);
         stageModel.getDefaultElementFactory().setup();
 
         board = stageModel.getBoard();
@@ -33,6 +34,12 @@ class TestKoRControllerMouse {
 
     @Test
     void testEmptySelection() {
+        KoRView view = Mockito.mock(KoRView.class);
+        KoRController controller = Mockito.mock(KoRController.class);
 
+        Pane pane = new Pane();
+        Mockito.when(view.getRootPane()).thenReturn(pane);
+
+        KoRControllerMouse mouse = new KoRControllerMouse(model, view, controller);
     }
 }
